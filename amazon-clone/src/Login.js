819b3dde656936a38ec27978a2b0ceefdf29d021
Anh <55465923+hixxixxi@users.noreply.github.com>
2021-05-05 00:41:54 -0600

    Add login page

---
+++ amazon-clone/src/Login.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Login.css";
+
+const signIn = (e) => {
+  e.preventDefault();
+
+  // firebase
+};
+
+const register = (e) => {
+  e.preventDefault();
+
+  //firebase
+};
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <div className="login">
+      <Link to="/">
+        <img
+          className="login__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+          alt=""
+        ></img>
+      </Link>
+      <div className="login__container">
+        <h1>Sign In</h1>
+        <form>
+          <h5>E-mail </h5>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></input>
+
+          <h5>Password</h5>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></input>
+
+          <button
+            type="submit"
+            onClick={signIn}
+            className="login__signInButton"
+          >
+            Sign in
+          </button>
+        </form>
+
+        <p>
+          By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use &
+          Sale. Please see our Privacy Notice, our Cookies Notice and our
+          Interest-Based Ads Notice.
+        </p>
+
+        <button onClick={register} className="login__registerButton">
+          Create your Amazon Account
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
--- amazon-clone/src/firebase.js
+++ amazon-clone/src/firebase.js
@@ -1,10 +1,14 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyDivPzebZxC_KU9lWqQMCGpaxdgZKHWUOE",
-    authDomain: "web-83042.firebaseapp.com",
-    projectId: "web-83042",
-    storageBucket: "web-83042.appspot.com",
-    messagingSenderId: "546299172139",
-    appId: "1:546299172139:web:d894766f21aa3221c9e16e",
-    measurementId: "G-4G3R9MNMJ0"
-  };
+  apiKey: "AIzaSyDivPzebZxC_KU9lWqQMCGpaxdgZKHWUOE",
+  authDomain: "web-83042.firebaseapp.com",
+  projectId: "web-83042",
+  storageBucket: "web-83042.appspot.com",
+  messagingSenderId: "546299172139",
+  appId: "1:546299172139:web:d894766f21aa3221c9e16e",
+  measurementId: "G-4G3R9MNMJ0",
+};
+
+const firebaseeApp = firebase.initializeApp(firebaseConfig);
